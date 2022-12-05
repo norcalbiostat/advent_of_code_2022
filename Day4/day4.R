eda9bf758b71386bfb3c2b33fde40c258f8a0aac
Donatello, Robin <rdonatello@csuchico.edu>
2022-12-05 07:46:06 -0800

    day4 part 1

---
+++ Day4/day4.R
@@ -0,0 +1,37 @@
+# Every section has a unique ID number
+# Each Elf is assigned a range of section IDs.
+# there are overlaps
+# In how many assignment pairs does one range fully contain the other?
+
+library(stringr) # coulda done it with substr if I wanted base only
+
+## Example list
+ex <- c(
+'2-4,6-8',
+'2-3,4-5',
+'5-7,7-9',
+'2-8,3-7',
+'6-6,4-6',
+'2-6,4-8',
+"8-50,49-82")
+
+# real list
+inlist <- readLines("Day4/input.txt")
+
+a <- str_split(inlist, ",|-" , simplify = TRUE)
+lst <- matrix(as.numeric(a), nrow=nrow(a), byrow=FALSE)
+
+# check for overlap
+olap <- (lst[,1] <= lst[,3] & lst[,2] >= lst[,4]) | # 1 fully contains 2
+        (lst[,3] <= lst[,1] & lst[,4] >= lst[,2]) | # 2 fully contains 1
+        (lst[,1] == lst[,3] & lst[,2] == lst[,4]) # same
+sum(olap)
+
+cbind(lst, len)
+# 359 (no)
+# 305 - yes
+
+# --- part 2 ---
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Advent of Code 2022
 
+This repository holds my code for my first ever Advent of Code, done completely in the R programming language. 
+
++-----------------------------------+------+
+| ## Day 1: Calorie Counting        | ⭐⭐ |
++===================================+======+
+| ## Day 2: Rock Paper Scissors     | ⭐⭐ |
++-----------------------------------+------+
+| ## Day 3: Rucksack Reorganization | ⭐⭐ |
++-----------------------------------+------+
+| ## Day 4: Camp Cleanup            |      |
++-----------------------------------+------+
+|                                   |      |
++-----------------------------------+------+
+|                                   |      |
++-----------------------------------+------+
